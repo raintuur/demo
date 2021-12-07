f009e8280d1103c2ac00e896a57b6d76ef6a2cad
Rain Tüür <rain.tuur@gmail.com>
2021-12-07 09:48:35 +0200

    WIP - updated VariableTypes and Person

--- src/main/java/VariableTypes.java
+++ src/main/java/VariableTypes.java
@@ -1,32 +1,32 @@
 public class VariableTypes {
     public static void main(String[] args) {
+// todo: https://www.geeksforgeeks.org/data-types-in-java/
 // todo: https://www.w3schools.com/java/java_data_types.asp
 // todo: https://programming.guide/java/primitives-vs-objects-references.html
 
 //todo:
-//      Peamised primitiivsed muutujad
+//      Peamised primitiivsed muutujad (prim. m. tüübid alati väikese tähega)
         int i = 17;                     // Integer (täisarv)
-        double myDoubleNumber = 5.99;   // Ujukomaarv; murdarv
-        char myLetter = 'D';            // Üksik täht
-        boolean myBool = true;          // Tõeväärtus (true or false)
-
+        double d = 5.99;                // Ujukomaarv; murdarv
+        char c = 'D';                   // Üksik täht
+        boolean b = true;               // Tõeväärtus (true or false)
 
 //todo:
 //      Java objektid: Stringid, Array, jne
-        String myText = "Hello";
-        int[] myArray = new int[]{1, 2, 3, 4};
+        String text = "Hello";
+        int[] array = new int[]{1, 3, 5, 7};
 
 //todo:
 //      Oma loodud objektid (klassid)
         Person p = new Person();
-        p.age = 34;
+        p.setName("Mari");
+        p.setAge(36);
 
 //todo:
 //      primitiivide ja objektide mälu kasutus
 //      primitiivide väärtused salvestatakse otse mällu
 //      objektide puhul salvestakse viited mälu asukohale
         System.out.println();
-
 
     }
 }
