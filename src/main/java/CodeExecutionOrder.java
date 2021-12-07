93dda09e6e72577703de7d33a3b430879b462092
Rain Tüür <rain.tuur@gmail.com>
2021-12-07 17:48:57 +0200

    WIP - updated CodeExecutionOrder

--- src/main/java/CodeExecutionOrder.java
+++ src/main/java/CodeExecutionOrder.java
@@ -5,26 +5,24 @@ public class CodeExecutionOrder {
     //  todo: 1) teeme ühe näidise, et näidata, et kuidas kood jookseb ülevalt->alla
     //  todo:    prindime lihtsalt konsooli mingit teksti
 
-    //  todo: 2) kasutame juurde ka teisi meetodeid printTextBlaBlaBla ja printTextEnglish
+    //  todo: 2) kasutame juurde ka teisi meetodeid printOtherText ja printTextEnglish
 
         System.out.println("Mingi tekst 1");
         System.out.println("Mingi tekst 2");
         System.out.println("Mingi tekst 3");
         System.out.println("Mingi tekst 4");
-        System.out.println("Mingi tekst 5");
-
     }
 
-    private static void printTextBlaBlaBla() {
-        System.out.println("bla bla bla 1");
-        System.out.println("bla bla bla 2");
-        System.out.println("bla bla bla 3");
+    private static void printOtherText() {
+        System.out.println("Mingi muu tekst 1");
+        System.out.println("Mingi muu tekst 2");
+        System.out.println("Mingi muu tekst 3");
     }
 
     private static void printTextEnglish() {
-        System.out.println("some text 1");
-        System.out.println("some text 2");
-        System.out.println("some text 3");
+        System.out.println("Some English text 1");
+        System.out.println("Some English text 2");
+        System.out.println("Some English text 3");
     }
 
 }
