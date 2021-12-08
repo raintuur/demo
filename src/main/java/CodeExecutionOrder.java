4eeeba8abd3a3a9d1bce32aa0e0e7890f88dd5a2
Rain Tüür <rain.tuur@gmail.com>
2021-12-08 08:39:01 +0200

    WIP - updated CodeExecutionOrder

--- src/main/java/CodeExecutionOrder.java
+++ src/main/java/CodeExecutionOrder.java
@@ -2,13 +2,16 @@ public class CodeExecutionOrder {
 
     public static void main(String[] args) {
 
-    //  todo: 1) teeme ühe näidise, et näidata, et kuidas kood jookseb ülevalt->alla
+    //  todo: 1) vaatame kuidas kood käivitub/jookseb alati suunaga ülevalt->alla
     //  todo:    prindime lihtsalt konsooli mingit teksti
 
     //  todo: 2) kasutame juurde ka teisi meetodeid printOtherText ja printTextEnglish
 
         System.out.println("Mingi tekst 1");
         System.out.println("Mingi tekst 2");
+
+        printTextEnglish();
+
         System.out.println("Mingi tekst 3");
         System.out.println("Mingi tekst 4");
     }
@@ -20,6 +23,7 @@ public class CodeExecutionOrder {
     }
 
     private static void printTextEnglish() {
+        printOtherText();
         System.out.println("Some English text 1");
         System.out.println("Some English text 2");
         System.out.println("Some English text 3");
