c3a2ed711ad199aca9e82ae993e513842c44d76e
Rain Tüür <rain.tuur@gmail.com>
2021-12-13 08:34:27 +0200

    WIP - added PrivatePublicMethodsDemo, SomeClass

---
+++ src/main/java/PrivatePublicMethodsDemo.java
@@ -0,0 +1,11 @@
+// TODO: https://ained.ttu.ee/javadoc/method/index.html#meetodi-ulesehitus
+
+public class PrivatePublicMethodsDemo {
+
+    public static void main(String[] args) {
+
+        SomeClass test = new SomeClass();
+        test.somePublicMethod();
+
+    }
+}
---
+++ src/main/java/SomeClass.java
@@ -0,0 +1,12 @@
+public class SomeClass {
+
+    public void somePublicMethod() {
+        System.out.println("See meetod on kõikjale nähtav");
+        somePrivateMethod();
+    }
+
+    private void somePrivateMethod() {
+        System.out.println("See meetod on nähtav vaid siin klassi sees");
+    }
+
+}
