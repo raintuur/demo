b5338b74a71866b83ca35cbc9d9fa367a7fc4471
Rain Tüür <rain.tuur@gmail.com>
2022-03-29 09:58:54 +0300

    WIP - updated MethodsDemo and added PrintToConsoleService

---
+++ src/main/java/PrintToConsoleService.java
@@ -0,0 +1,12 @@
+
+public class PrintToConsoleService {
+
+    public void printGreeting(String name) {
+        System.out.println("Hello, my name is " + name);
+    }
+
+    public void printGreetingInEstonian(String name) {
+        System.out.println("Tere, minu nimi on " + name);
+    }
+
+}
